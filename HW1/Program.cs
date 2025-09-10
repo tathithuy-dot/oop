c442c8c38a74d185a86f3c31a2cadf193ff7534d
tathithuy-dot <tathithuytathithuy12@gmail.com>
2025-09-10 20:55:47 +0700

    Update Program.cs

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -6,5 +6,6 @@
         MyDate md1 = new MyDate(DateTime.Now.Day,DateTime.Now.Month, DateTime.Now.Year);
         Console.WriteLine("{0}/{1}/{2}",md1.Day,md1.Month,md1.Year);
         Console.ReadKey();
+        Console.WriteLine();
     }
 }
