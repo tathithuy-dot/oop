81f597ff6f1f71a718b95b5424e086d910f5fa72
tathithuy-dot <tathithuytathithuy12@gmail.com>
2025-09-11 09:01:12 +0700

    Update Program.cs

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,4 +1,10 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program
 {
     
     private static void Main(string[] args)
