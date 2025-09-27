9ed6edb6559cd0b4c49750b5d424ba6b92026b66
tathithuy-dot <tathithuytathithuy12@gmail.com>
2025-09-27 22:44:42 +0700

    Update MayLanh.cs

--- HW4/MayLanh.cs
+++ HW4/MayLanh.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace HW4
 {
-    internal class MayLanh: ThietBi
+    internal class MayLanh: ThietBi //Kế thừa
     {
-        public double MaLuc { get; set; }
-        public override void Nhap()
+        public double MaLuc { get; set; }// Thêm thuộc tính mới, kế thừa những thuộc tính của ThietBi
+        //Nhập thông tin của lớp MayLanh (những thông tin như nhà sản xuất, model, giá được kế thừa từ lớp ThietBi)
+        public override void Nhap() //Dùng override ghi đè
         {
             Console.Write("Nhap Nha San Xuat: ");
             NhaSX = Console.ReadLine();
@@ -20,7 +21,8 @@ namespace HW4
             Console.Write("Nhap Ma Luc: ");
             MaLuc = Convert.ToDouble(Console.ReadLine());
         }
-        public override void Xuat()
+        // Xuất thông tin máy lạnh
+        public override void Xuat() //Dùng override ghi đè
         {
             Console.WriteLine("Thong Tin May Lanh:");
             Console.WriteLine("Nha San Xuat: " + NhaSX);
@@ -30,3 +32,4 @@ namespace HW4
         }
     }
 }
+
