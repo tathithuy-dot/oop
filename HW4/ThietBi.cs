ff527e8543e6511375bd4c1b8b3f384133efa5b6
tathithuy-dot <tathithuytathithuy12@gmail.com>
2025-09-27 22:51:03 +0700

    Update ThietBi.cs

--- HW4/ThietBi.cs
+++ HW4/ThietBi.cs
@@ -4,24 +4,24 @@ using System.Text;
 
 namespace HW4
 {
-   internal abstract class  ThietBi
+   internal abstract class  ThietBi // Lớp trừu tượng, bắt buộc các lớp khác kế thừa
 {
-        public string NhaSX { get; set; }
+        public string NhaSX { get; set; } // Do dùng thuộc tính nên vẫn có thể để public cho tiện nhập/ xuất
         public string Model { get; set; }
         public double Gia { get; set; }
-        public ThietBi()
+        public ThietBi() // Constructor mặc định
         {
-
         }
-        public ThietBi(string nhasx, string model, double gia)
+        public ThietBi(string nhasx, string model, double gia) // Constructor có tham số
         {
             NhaSX = nhasx;
             Model = model;
             Gia = gia;
         }
-        public abstract void Nhap();
-        public abstract void Xuat();
+        public abstract void Nhap(); // Phương thức trừu tượng, bắt buộc phải kế thừa
+        public abstract void Xuat(); //Phương thức trừu tượng, bắt buộc phải kế thừa
     }
 }
+
 
 
