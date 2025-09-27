ee6d590c26b528431907e6f635b6d5a7b62618ed
tathithuy-dot <tathithuytathithuy12@gmail.com>
2025-09-27 22:42:53 +0700

    Update MayGiat.cs

--- HW4/MayGiat.cs
+++ HW4/MayGiat.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace HW4
 {
-    internal class MayGiat: ThietBi
+    internal class MayGiat: ThietBi // Kế thừa
     {
-        public double TrongLuongMax { get; set; }
-        public override void Nhap()
+        public double TrongLuongMax { get; set; } // Thêm thuộc tính mới, kế thừa những thuộc tính của ThietBi
+        //Nhập thông tin của lớp MayGiat (những thông tin như nhà sản xuất, model, giá được kế thừa từ lớp ThietBi)
+        public override void Nhap() //Dùng override ghi đè
         {
             Console.Write("Nhap Nha San Xuat: ");
             NhaSX = Console.ReadLine();
@@ -20,7 +21,8 @@ namespace HW4
             Console.Write("Nhap Trong Luong toi da: ");
             TrongLuongMax = Convert.ToDouble(Console.ReadLine());
         }
-        public override void Xuat()
+        //Xuất thông tin máy giặt
+        public override void Xuat() // Dùng override ghi đè
         {
             Console.WriteLine("Thong Tin May Giat:");
             Console.WriteLine("Nha San Xuat: " + NhaSX);
@@ -30,3 +32,4 @@ namespace HW4
         }
     }
 }
+
