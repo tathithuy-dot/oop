0bcf33cb26c6f0c1a86d306dd2de00c36e3dbfd8
tathithuy-dot <tathithuytathithuy12@gmail.com>
2025-10-02 21:11:22 +0700

    Update and rename MayGiat.cs to Phong.cs

---
+++ HW4/Phong.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4
+{
+        //Chuong trinh tinh tien thue phong khach san
+    abstract class Phong //Xay dung lop truu tuong Phong
+    {
+        protected int songay;// Thanh phan du lieu
+        public Phong(int songay) //Phuong thuc khoi tao co tham so
+        {
+            this.songay = songay;
+        }
+        public abstract double TinhTien(); // Phuong thuc truu tuong tinh tien, bat cac class con phai override
+        public abstract void Hien(); //Phuong thuc truu tuong hien thi, bat cac class con phai override
+    }
+}
+
+
