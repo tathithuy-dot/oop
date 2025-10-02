46c5134f3e46ee00c755cf08f5fabd74a1001aff
tathithuy-dot <tathithuytathithuy12@gmail.com>
2025-10-02 21:13:39 +0700

    Update and rename ThietBi.cs to PhongB.cs

---
+++ HW4/PhongB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW4
+{
+      class PhongB : Phong//Class Phong B ke thua tu class Phong
+   {
+       public PhongB(int songay) : base(songay)//Ke thua het cac thanh phan du lieu cua class Phong
+       {
+       }
+       public override double TinhTien()//Phuong thuc tinh tien duoc ghi de
+       {
+           if (songay < 5) return songay * 60;
+           else return 5 * 60 + (songay - 5) * 60 * 0.9;
+       }
+       public override void Hien()//Phuong thuc hien thi duoc ghi de
+       {
+           Console.WriteLine("Dich vu phong B");
+           Console.WriteLine("Tien phong: " + TinhTien());
+       }
+   }
+}
+
+
+
+
