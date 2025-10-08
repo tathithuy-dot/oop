f11805ffaf69a22ea02c8c682fe1ad15f54b57bc
tathithuy-dot <tathithuytathithuy12@gmail.com>
2025-10-08 21:45:47 +0700

    Update Yamada.cs

--- HW5/Yamada.cs
+++ HW5/Yamada.cs
@@ -5,7 +5,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Cuahangxe
 {
     internal class Yamada
     {
@@ -106,3 +106,4 @@ namespace ConsoleApp1
         }
     }
 }
+
